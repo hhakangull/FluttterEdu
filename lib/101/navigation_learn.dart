417623d5ee75d550e6fe980503301fce375c948c
Hakan GÜL <hakanngul@icloud.com>
2022-06-19 20:48:38 +0300

    navigationManager

--- lib/101/navigation_learn.dart
+++ lib/101/navigation_learn.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'navigate_detail_learn.dart';
+
 class NavigationLearn extends StatefulWidget {
   const NavigationLearn({Key? key}) : super(key: key);
 
@@ -7,9 +9,66 @@ class NavigationLearn extends StatefulWidget {
   _NavigationLearnState createState() => _NavigationLearnState();
 }
 
-class _NavigationLearnState extends State<NavigationLearn> {
+class _NavigationLearnState extends State<NavigationLearn>
+    with NavigatorManager {
+  List<int> selectedItems = [];
+  void addSelected(int index, bool isAdd) {
+    setState(() {
+      isAdd ? selectedItems.add(index) : selectedItems.remove(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          onPressed: () async {}, child: const Icon(Icons.forward)),
+      body: ListView.builder(itemBuilder: (context, index) {
+        return TextButton(
+          onPressed: () async {
+            final response = await navigateToWidgetNormal<bool>(
+                context,
+                NavigateDetailLearn(
+                  isOkey: selectedItems.contains(index),
+                ));
+            if (response is bool) {
+              addSelected(index, response);
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Placeholder(
+              color: selectedItems.contains(index) ? Colors.green : Colors.red,
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
+
+mixin NavigatorManager {
+  void navigateToWidget(BuildContext context, Widget widget) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return widget;
+        },
+        fullscreenDialog: true,
+        settings: const RouteSettings(),
+      ),
+    );
+  }
+
+  Future<T?> navigateToWidgetNormal<T>(BuildContext context, Widget widget) {
+    return Navigator.of(context).push<T>(
+      MaterialPageRoute(
+        builder: (context) {
+          return widget;
+        },
+        fullscreenDialog: true,
+        settings: const RouteSettings(),
+      ),
+    );
   }
 }
